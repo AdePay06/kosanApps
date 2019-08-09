26d28ace69ceec9021495b5f2afbd543453b057b
Ade Priyanto <ade.priyanto06@gmail.com>
2019-08-09 11:14:29 +0700

    first commit

---
+++ src/main/java/com/ade/priyanto/apps/kosanApps/KosanAppsApplication.java
@@ -0,0 +1,13 @@
+package com.ade.priyanto.apps.kosanApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosanAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosanAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ade/priyanto/apps/kosanApps/KosanAppsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ade.priyanto.apps.kosanApps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KosanAppsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
